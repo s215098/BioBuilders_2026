b59f4daf6586edd8a4caaf24fd7eaffe48d78ed9
Kristine Toft <s215098@dtu.dk>
2026-04-29 01:05:47 +0200

    changed docking_vina to also handle SMILES

--- 00_InBetween/molecular_docking/src/docking_vina.py
+++ 00_InBetween/molecular_docking/src/docking_vina.py
@@ -1,4 +1,5 @@
 import argparse
+from ast import arg
 import os
 import numpy as np
 from pathlib import Path
@@ -7,26 +8,46 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from meeko import MoleculePreparation
 
-def get_receptor_center_and_size(pdb_file):
+# def get_receptor_center_and_size(pdb_file):
+#     """Calculates the center and bounding box size of the receptor for blind docking."""
+#     coordinates = []
+#     with open(pdb_file, 'r') as f:
+#         for line in f:
+#             if line.startswith("ATOM") or line.startswith("HETATM"):
+#                 if line[16] == 'A' or line[16] == ' ' :
+#                     x = float(line[30:38])
+#                     y = float(line[38:46])
+#                     z = float(line[46:54])
+#                     coordinates.append([x, y, z])
+    
+#     coords = np.array(coordinates)
+#     center = np.mean(coords, axis=0)
+    
+#     # (Max - Min) per axis gives the full span of the protein
+#     size = np.max(coords, axis=0) - np.min(coords, axis=0)
+    
+#     # Add a 10 Angstrom buffer
+#     size = size + 10.0
+    
+#     return center.tolist(), size.tolist()
+
+def get_receptor_center_and_size(pdbqt_file):
     """Calculates the center and bounding box size of the receptor for blind docking."""
     coordinates = []
-    with open(pdb_file, 'r') as f:
+    with open(pdbqt_file, 'r') as f:
         for line in f:
             if line.startswith("ATOM") or line.startswith("HETATM"):
-                if line[16] == 'A' or line[16] == ' ' :
+                try:
                     x = float(line[30:38])
                     y = float(line[38:46])
                     z = float(line[46:54])
                     coordinates.append([x, y, z])
+                except ValueError:
+                    continue  # skip malformed lines
     
     coords = np.array(coordinates)
     center = np.mean(coords, axis=0)
-    
-    # (Max - Min) per axis gives the full span of the protein
-    size = np.max(coords, axis=0) - np.min(coords, axis=0)
-    
-    # Add a 10 Angstrom buffer
-    size = size + 10.0
+    size = np.max(coords, axis=0) - np.min(coords, axis=0) + 10.0
     
     return center.tolist(), size.tolist()
 
@@ -35,9 +56,12 @@ def main():
     
     # Required Arguments
     parser.add_argument('-r', '--receptor', required=True, help="Path to the receptor PDBQT file")
-    parser.add_argument('-l', '--ligand', required=True, help="Path to the ligand SDF file")
     parser.add_argument('-o', '--out_dir', required=True, help="Directory to save the resulting PDBQT poses")
     
+    # Adding optional argument for docking with SMILES string of the ligand
+    parser.add_argument('-l', '--ligand', required=False, help="Path to the ligand SDF file")
+    parser.add_argument('-ls', '--ligand_smiles', required=False, help="SMILES string of the ligand (optional)")
+
     # Optional Arguments for Targeted Docking
     parser.add_argument('--center', type=float, nargs=3, help="Center coordinates (X Y Z). If omitted, blind docking is triggered.")
     parser.add_argument('--box_size', type=float, nargs=3, default=[15.0, 15.0, 15.0], help="Box dimensions (X Y Z). Default: 15 15 15. Used only if --center is provided.")
@@ -51,11 +75,16 @@ def main():
     # 1. Setup paths and output directory
     os.makedirs(args.out_dir, exist_ok=True)
     rec_path = Path(args.receptor)
-    lig_path = Path(args.ligand)
+    
+    if args.ligand_smiles:
+        lig_name = Chem.MolToInchiKey(Chem.MolFromSmiles(args.ligand_smiles))[:8]
+
+    else: 
+        lig_path = Path(args.ligand)
+        lig_name = lig_path.stem
     
     # Extract base names for dynamic file naming (e.g., '6T0Y' and 'BADGE')
     rec_name = rec_path.stem.replace('_clean_h_meeko', '') 
-    lig_name = lig_path.stem
 
     # 2. Determine Mode & Grid Box Parameters
     if args.center:
@@ -88,11 +117,18 @@ def main():
 
     # 4. Prepare Ligand (RDKit -> Meeko)
     print("Preparing ligand with RDKit and Meeko...")
-    suppl = Chem.SDMolSupplier(str(lig_path))
-    mol = next(suppl)
-    if mol is None:
-        raise ValueError(f"Failed to read a valid molecule from {lig_path}")
-    
+
+    if args.ligand_smiles:
+        mol = Chem.MolFromSmiles(args.ligand_smiles)
+        if mol is None:
+            raise ValueError(f"Failed to parse SMILES: {args.ligand_smiles}")
+
+    else:
+        suppl = Chem.SDMolSupplier(str(lig_path))
+        mol = next(suppl)
+        if mol is None:
+            raise ValueError(f"Failed to read a valid molecule from {lig_path}")
+        
     mol_with_h = Chem.AddHs(mol)
     AllChem.EmbedMolecule(mol_with_h, AllChem.ETKDG())
     AllChem.MMFFOptimizeMolecule(mol_with_h)
